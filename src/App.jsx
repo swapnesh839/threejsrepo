2913f7d54e4348fa47345753a614e1b10e70e07e
swapnesh <swapnesha839@gmail.com>
2024-02-07 15:39:57 +0530

    "2nd trial"

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { FaChevronLeft } from "react-icons/fa";
-import { FaChevronRight } from "react-icons/fa";
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Component1 from './Component/Component1'
 
 const App = () => {
-  const renderDivRef = useRef(null);
-  const clock = new THREE.Clock();
-  const [color, setColor] = useState(false)
-  const [count, setCount] = useState(0)
-
-  function countValue(value) {
-    if (value === "increment") {
-      setCount((prev) => {
-        const newCount = prev + 1;
-        if (newCount === 2) {
-          return 0;
-        }
-        return newCount;
-      });
-    } else {
-      setCount((prev) => {
-        const newCount = prev - 1;
-        if (newCount === -1) {
-          return 2;
-        }
-        return newCount;
-      });
-    }
-  }
-  
-
-
-  useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      renderDivRef.current.offsetWidth / renderDivRef.current.offsetHeight,
-      0.1,
-      1000
-    );
-    camera.position.set(5, 3, 8);
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(
-      renderDivRef.current.offsetWidth,
-      renderDivRef.current.offsetHeight
-    );
-    renderer.setClearColor("#6B6B6B", 1);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    let loadedobj;
-
-
-    const Loader = new GLTFLoader();
-
-    let modelPath 
-
-    switch (count) {
-      case 0:
-        // modelPath = "../src/assets/LOT(308).glb";
-        modelPath = "https://codekart.s3.amazonaws.com/Recline_Sofa+(1).glb";
-        break;
-      case 1:
-        modelPath = "https://codekart.s3.amazonaws.com/White_Sofa_Smooth_Fabric.glb";
-        // modelPath = "../src/assets/LOT(308).glb";
-        break;
-    }
-
-
-
-
-    Loader.load(modelPath, (gltf) => {
-      loadedobj = gltf.scene;
-      loadedobj.scale.set(3, 3, 3);
-
-      scene.add(loadedobj);
-    });
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1); 
-    scene.add(ambientLight);
-    renderDivRef.current.appendChild(renderer.domElement);
-
-    function animate() {
-      requestAnimationFrame(animate);
-      // const delta = clock.getDelta();
-
-      controls.update();
-      renderer.render(scene, camera);
-    }
-
-    animate();
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    scene.add(directionalLight);
-
-    return () => {
-      renderDivRef.current.removeChild(renderer.domElement);
-      controls.dispose();
-    };
-  }, [color, count]);
-
-
   return (
-    <>
-      <div className="w-50 vh-100 position-relative start-50 translate-middle-x top-50">
-        <button onClick={() => { countValue("increment") }} className="position-absolute btn btn-success end-0 top-50">
-          <FaChevronRight />
-        </button>
-        <button onClick={() => { countValue("decrement") }} className="position-absolute btn btn-success top-50">
-          <FaChevronLeft />
-        </button>
-        <div
-          className="m-auto ratio-1x1 h-100"
-          ref={renderDivRef}
-        />
-      </div>
-    </>
-  );
-};
+    <div className=" bg-info-subtle w-100 vh-100 d-flex">
+      <Container style={{width:"640px", height:"100vh"}} className="m-auto bg-black">
+        <Component1/>
+      </Container>
+    </div>
+  )
+}
 
-export default App;
+export default App
---
+++ src/Component/Component1.jsx
@@ -0,0 +1,71 @@
+import React, { Suspense, useEffect, useRef, useState } from 'react'
+import { Canvas,useFrame } from '@react-three/fiber';
+import { OrbitControls, PerspectiveCamera, Plane, Sphere, useGLTF } from '@react-three/drei'
+import { FaAngleLeft } from "react-icons/fa6";
+import { FaAngleRight } from 'react-icons/fa6';
+
+
+// const angleradiant = (deg) => (Math.PI / 100) * deg
+
+const Cube = ({ color, position, size }) => {
+  const meshref = useRef(null)
+  useFrame((state, delta) => {
+    // console.log(state,delta);
+    meshref.current.rotation.y += delta
+    meshref.current.rotation.x += delta
+    meshref.current.rotation.z += delta
+  })
+  return (
+    <mesh position={position} ref={meshref}>
+      <boxGeometry args={size} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  )
+}
+
+const ImportedFile = (url) => {
+  console.log(url);
+
+  useEffect(()=>{
+  },[url])
+  const { scene } = useGLTF(url.url);
+  useFrame((state, delta) => {
+  ImportedFileref.current.rotation.y += delta
+  })
+  const ImportedFileref = useRef(null)
+
+  return (
+    <>
+      <primitive object={scene} ref={ImportedFileref} scale={[2, 2, 2]} />
+    </>
+  )
+};
+
+const Component1 = () => {
+  const [count, setCount] = useState(true)
+  // const btnref = useRef(null)
+  const toggleModel = () => {
+    setCount((e) => !e)
+  }
+  return (
+    <div className="position-relative w-100 h-100">
+      <Canvas className="h-100 w-100 position-relative">
+        <PerspectiveCamera makeDefault position={[5, 2, 4]} />
+        <Suspense fallback={null}>
+          <OrbitControls />
+          <ImportedFile url={(count)?"https://codekart.s3.amazonaws.com/Recline_Sofa+(1).glb":"https://codekart.s3.amazonaws.com/White_Sofa_Smooth_Fabric.glb"} />
+        </Suspense>
+        <directionalLight color={"bad4f9"} intensity={6} />
+        <ambientLight args={["white", 1]} />
+      </Canvas>
+      <button onClick={toggleModel} className="position-absolute start-0 btn btn-success top-50" >
+        <FaAngleLeft/>
+      </button>
+      <button onClick={toggleModel} className="position-absolute end-0 btn btn-success top-50" >
+        <FaAngleRight/>
+      </button>
+    </div>
+  )
+}
+
+export default Component1
